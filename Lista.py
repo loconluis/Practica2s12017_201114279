66851f24a5698b564376f2ef47d7bf59d78b14af
loconluis <luis.locon@gmail.com>
2017-02-27 18:16:52 -0600

    Metodos de la lista simple

--- Lista.py
+++ Lista.py
@@ -47,18 +47,40 @@ class Lista():
         nuevo.setDato(dato)
         if self.estaVacia() is True:
             self.indice = nuevo
-            self.indice.setSig(None)
+            self.size = self.size + 1
         else:
             temp = Nodo()
             temp = self.indice
             while temp.getSig() is not None:
                 temp = temp.getSig()
             temp.setSig(nuevo)
+            self.size = self.size + 1
+
+    def buscar(self, dato):
+        """Metodo Buscar."""
+        temp = Nodo()
+        temp = self.indice
+        i = 1
+        while temp is not None:
+            if temp.getDato() is dato:
+                print "Palabra: "+temp.getDato()+" en la posicion: "+str(i)
+                break
+            else:
+                temp = temp.getSig()
+            i = i + 1
+
+    def longitud(self):
+        """Retorna size."""
+        value = self.size
+        return value
 
     def verLista(self):
         """Imprime la lista."""
-        aux = Nodo()
-        aux = self.indice
-        while aux.getSig() is not None:
-            print aux.getDato()
-            aux = aux.getSig()
+        if self.estaVacia() is False:
+            aux = Nodo()
+            aux = self.indice
+            while aux is not None:
+                print aux.getDato()
+                aux = aux.getSig()
+        else:
+            print "No te salio"
--- prueba.py
+++ prueba.py
@@ -8,5 +8,17 @@ list.agregar("c")
 list.agregar("d")
 list.agregar("e")
 list.agregar("f")
+list.agregar("g")
+list.agregar("h")
+list.agregar("i")
+list.agregar("j")
+list.agregar("k")
+list.agregar("l")
+
 
 list.verLista()
+
+
+list.buscar("f")
+
+print "La lista tiene longitud de : "+str(list.longitud())
