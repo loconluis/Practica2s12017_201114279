ee774e4dde8c2b99aeb1e23119ab9e91ca22bcc9
loconluis <luis.locon@gmail.com>
2017-02-27 19:15:47 -0600

    Agregada la Estructura de cola

---
+++ Cola.py
@@ -0,0 +1,80 @@
+"""Cola."""
+
+
+class NodoC():
+    """Clase Nodo de la cola."""
+
+    def __init__(self):
+        """Constructor."""
+        self.dato = None
+        self.sig = None
+
+    def getDato(self):
+        """Retorna el dato."""
+        return self.dato
+
+    def setDato(self, dat):
+        """Setea el dato."""
+        self.dato = dat
+
+    def getSig(self):
+        """Retorna el siguiente."""
+        return self.sig
+
+    def setSig(self, siguiente):
+        """Setea el nodo siguiente."""
+        self.sig = siguiente
+
+
+class Cola():
+    """Clase de la Cola."""
+
+    def __init__(self):
+        """Constructor."""
+        self.cabeza = None
+        self.cola = None
+        self.size = 0
+
+    def estaVacia(self):
+        """Verifica si esta vacia la cola."""
+        if self.cabeza is None:
+            return True
+        else:
+            return False
+
+    def push(self, dato):
+        """Agrega a la cola."""
+        nuevo = NodoC()
+        nuevo.setDato(dato)
+        if self.estaVacia() is True:
+            self.cabeza = nuevo
+            self.cola = self.cabeza
+        else:
+            temp = NodoC()
+            temp = self.cabeza
+            while temp.getSig() is not None:
+                temp = temp.getSig()
+            temp.setSig(nuevo)
+            self.cola = nuevo
+
+    def pop(self):
+        """Elimina primero de la cola."""
+        if self.cabeza is not None:
+            temp = NodoC()
+            temp = self.cabeza
+            self.cabeza = temp.getSig()
+            temp.setSig(None)
+            return temp.getDato()
+        else:
+            return "Lista vacia"
+
+    def verCola(self):
+        """Imprime la cola."""
+        if self.estaVacia() is False:
+            aux = NodoC()
+            aux = self.cabeza
+            while aux is not None:
+                print aux.getDato()
+                aux = aux.getSig()
+        else:
+            print "No te salio"
--- prueba.py
+++ prueba.py
@@ -1,24 +1,38 @@
 """Prueba de Estructuras."""
-from Lista import Lista
+# from Lista import Lista
+from Cola import Cola
 
-list = Lista()
-list.agregar("a")
-list.agregar("b")
-list.agregar("c")
-list.agregar("d")
-list.agregar("e")
-list.agregar("f")
-list.agregar("g")
-list.agregar("h")
-list.agregar("i")
-list.agregar("j")
-list.agregar("k")
-list.agregar("l")
+# list = Lista()
+cola = Cola()
+# list.agregar("a")
+# list.agregar("c")
+# list.agregar("d")
+# list.agregar("b")
+# list.agregar("e")
+# list.agregar("f")
+# list.agregar("g")
+# list.agregar("h")
+# list.agregar("i")
+# list.agregar("j")
+# list.agregar("k")
+# list.agregar("l")
+cola.push(10)
+cola.push(20)
+cola.push(30)
+cola.push(40)
+cola.push(50)
+# list.verLista()
+cola.verCola()
+print "*****************************"
 
+print "Se elimino: "+str(cola.pop())
 
-list.verLista()
+cola.verCola()
+print "*****************************"
 
+print "Se elimino: "+str(cola.pop())
 
-list.buscar("f")
+cola.verCola()
+# list.buscar("f")
 
-print "La lista tiene longitud de : "+str(list.longitud())
+# print "La lista tiene longitud de : "+str(list.longitud())
